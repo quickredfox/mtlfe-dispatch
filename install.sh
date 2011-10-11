de9a07119dfc0df1ec16d35073fe0a492c35e838
Quickredfox <code@quickredfox.at>
2011-10-11 14:40:32 -0400

    server confs

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
-cp /var/mtlfe/dispatch/config/upstart/mtlfe-server-8001.conf /etc/init/mtlfe-server-8001.conf
-cp /var/mtlfe/dispatch/config/upstart/mtlfe-server-8002.conf /etc/init/mtlfe-server-8002.conf
-cp /var/mtlfe/dispatch/config/upstart/mtlfe-server-8003.conf /etc/init/mtlfe-server-8003.conf
-cp /var/mtlfe/dispatch/config/upstart/mtlfe-hook-9001.conf   /etc/init/mtlfe-hook-9001.conf
+cp /var/mtlfe/dispatch/config/upstart/mtlfe-server-8001.conf /etc/init/mtlfe-server-8001.conf && \
+cp /var/mtlfe/dispatch/config/upstart/mtlfe-server-8002.conf /etc/init/mtlfe-server-8002.conf && \
+cp /var/mtlfe/dispatch/config/upstart/mtlfe-server-8003.conf /etc/init/mtlfe-server-8003.conf && \
+cp /var/mtlfe/dispatch/config/upstart/mtlfe-hook-9001.conf   /etc/init/mtlfe-hook-9001.conf   && \
 
-cp /var/mtlfe/dispatch/config/monit/monitrc /etc/monit/monitrc
-cp /var/mtlfe/dispatch/config/nginx/mtlfe.org /etc/nginx/sites-available/mtlfe.org
+cp /var/mtlfe/dispatch/config/monit/monitrc /etc/monit/monitrc && \
+cp /var/mtlfe/dispatch/config/nginx/mtlfe.org /etc/nginx/sites-available/mtlfe.org && \
 
-rm -rf /etc/nginx/sites-enabled/mtlfe.org
-ln -s /etc/nginx/sites-available/mtlfe.org /etc/nginx/sites-enabled/mtlfe.org 
+rm -rf /etc/nginx/sites-enabled/mtlfe.org && \
+ln -s /etc/nginx/sites-available/mtlfe.org /etc/nginx/sites-enabled/mtlfe.org && \
 
-start mtlfe-server-8001.conf
-start mtlfe-server-8002.conf
-start mtlfe-server-8003.conf
+start mtlfe-server-8001.conf && \
+start mtlfe-server-8002.conf && \
+start mtlfe-server-8003.conf && \
 start mtlfe-server-9001.conf
 
 # /etc/init.d/nginx restart &&\
