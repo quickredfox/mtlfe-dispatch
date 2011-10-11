f32437633c61811501926ef19f15e46c2dba5ffd
Quickredfox <code@quickredfox.at>
2011-10-11 14:50:36 -0400

    server confs

---
+++ uninstall.sh
@@ -0,0 +1,15 @@
+stop mtlfe-server-8001 
+stop mtlfe-server-8002 
+stop mtlfe-server-8003 
+stop mtlfe-hook-9001
+
+rm /etc/init/mtlfe-server-8001.conf 
+rm /etc/init/mtlfe-server-8002.conf 
+rm /etc/init/mtlfe-server-8003.conf 
+rm /etc/init/mtlfe-hook-9001.conf   
+
+rm /etc/monit/monitrc
+rm  /etc/nginx/sites-available/mtlfe.org
+
+rm -rf /etc/nginx/sites-enabled/mtlfe.org
+
