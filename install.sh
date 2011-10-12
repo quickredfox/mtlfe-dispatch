16b0a3efac728c1e61ad0effc1e7ebe1d60ea4ca
Quickredfox <code@quickredfox.at>
2011-10-12 15:24:40 -0400

    dev

--- install.sh
+++ install.sh
@@ -6,9 +6,6 @@ cp /var/mtlfe/dispatch/config/upstart/mtlfe-hook-9001.conf   /etc/init/mtlfe-hoo
 cp /var/mtlfe/dispatch/config/monit/monitrc /etc/monit/monitrc && \
 cp /var/mtlfe/dispatch/config/nginx/mtlfe.org /etc/nginx/sites-available/mtlfe.org && \
 
-
-chmod 0644 /etc/monit/monitrc
-
 rm -rf /etc/nginx/sites-enabled/mtlfe.org && \
 ln -s /etc/nginx/sites-available/mtlfe.org /etc/nginx/sites-enabled/mtlfe.org && \
 
@@ -18,5 +15,5 @@ start mtlfe-server-8003 && \
 start mtlfe-hook-9001
 
 /etc/init.d/nginx restart &&\
-monit -d 60 -c /etc/monit/monitrc
+#monit -d 60 -c /etc/monit/monitrc
 
